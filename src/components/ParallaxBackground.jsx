e04c960665aaf5a1b7c9a00ba42e3d9391d283c4
Alin Oancea <alin.oancea@keepcalling.com>
2025-09-02 15:24:47 +0300

    get rid of the parallax effect

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -3,9 +3,9 @@ import { motion, useScroll, useSpring, useTransform } from "motion/react";
 const ParallaxBackground = () => {
   const { scrollYProgress } = useScroll();
   const x = useSpring(scrollYProgress, { damping: 50 });
-  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "70%"]);
-  const planetsX = useTransform(x, [0, 0.5], ["0%", "-20%"]);
-  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"]);
+  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
+  const planetsX = useTransform(x, [0, 0.5], ["0%", "0%"]);
+  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
   const mountain1Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
 
   return (
