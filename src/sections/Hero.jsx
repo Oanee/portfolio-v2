c633f59e243c52a834fc9e39b64b7471e81cd7e3
Alin Oancea <alin.oancea@keepcalling.com>
2025-09-02 15:42:46 +0300

    comment parallax

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-dvh overflow-hidden c-space">
       <HeroText />
-      <ParallaxBackground />
+      {/*<ParallaxBackground />*/}
       <figure className="absolute inset-0 w-[100dvw] h-[100dvh]">
         <Canvas camera={{ position: [0, 1, 3] }}>
           <Suspense fallback={<Loader />}>
