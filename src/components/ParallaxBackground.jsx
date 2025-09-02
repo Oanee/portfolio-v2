23a29e7c46d64fbd9ab116d5cc055b8b9483d966
Alin Oancea <alin.oancea@keepcalling.com>
2025-09-02 16:23:57 +0300

    change parallax speed

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -3,9 +3,9 @@ import { motion, useScroll, useSpring, useTransform } from "motion/react";
 const ParallaxBackground = () => {
   const { scrollYProgress } = useScroll();
   const x = useSpring(scrollYProgress, { damping: 50 });
-  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "70%"]);
-  const planetsX = useTransform(x, [0, 0.5], ["0%", "-20%"]);
-  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"]);
+  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "140%"]);
+  const planetsX = useTransform(x, [0, 0.5], ["0%", "-40%"]);
+  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "60%"]);
   const mountain1Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
 
   return (
