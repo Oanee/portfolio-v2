b6935c12b4e81b5fb5019a2830d2899b4332fd5e
Alin Oancea <alin.oancea@keepcalling.com>
2025-09-02 15:47:20 +0300

    get rid of classes for testing 2

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
-    <div className="container mx-auto max-w-7xl">
+    <div className="">
       <Navbar />
       <Hero />
       <About />
