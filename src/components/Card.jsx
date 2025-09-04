3b977ecfab9f19473a9d11e3f38933aaf97cd5b6
Alin Oancea <alin.oancea@keepcalling.com>
2025-09-04 13:43:14 +0300

    fix about section bug part 9

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,18 @@
 import { motion } from "motion/react";
+import { useState } from "react";
 
 const Card = ({ style, text, image, containerRef }) => {
-  const rotateValue = parseFloat(style.rotate || 0);
-
-  const transformStyle = {
-    ...style,
-    transform: `rotate(${rotateValue}deg) translateZ(0)`,
-  };
+  const [isPressed, setIsPressed] = useState(false);
+  const rotate = `rotate-${style.rotate}`;
+  console.log(containerRef);
 
   return image && !text ? (
     <motion.img
+      onDrag={() => setIsPressed(true)}
       src={image}
       alt="card"
-      className="absolute w-12 cursor-grab md:scale-125"
-      style={transformStyle}
+      className={`absolute w-12 cursor-grab md:scale-125 ${!isPressed && rotate}`}
+      style={style}
       whileHover={{ scale: 1.1 }}
       drag
       dragConstraints={containerRef}
@@ -21,8 +20,9 @@ const Card = ({ style, text, image, containerRef }) => {
     />
   ) : (
     <motion.p
-      className="absolute px-1 py-4 text-sm md:text-lg text-center rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab"
-      style={transformStyle}
+      onDrag={() => setIsPressed(true)}
+      className={`absolute px-1 py-4 text-sm md:text-lg text-center rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab ${!isPressed && rotate}`}
+      style={style}
       whileHover={{ scale: 1.1 }}
       drag
       dragConstraints={containerRef}
